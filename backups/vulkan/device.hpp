a20bda66a33d487871e230e5b7b908933c3cb8c8
InsideBSI <insidebsiprod@gmail.com>
2024-02-07 23:25:41 +0100

    cleanup

--- backups/vulkan/device.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace EternityVoxelEngine {
-	class EveDevice {
-		public:
-
-		private:
-	};
-}
--- backups/vulkan/pipeline.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace EternityVoxelEngine {
-	class EvePipeline {
-		public:
-
-		private:
-	};
-}
