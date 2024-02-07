e8544259fc617e7185ebcf13aac30761a98b0c22
InsideBSI <insidebsiprod@gmail.com>
2024-02-07 19:28:58 +0100

    3D update

--- src/app.cpp
+++ src/app.cpp
@@ -37,21 +37,71 @@ namespace eve {
 		vkDeviceWaitIdle(eveDevice.device());
 	}
 
-	void App::loadGameObjects() {
-		std::vector<EveModel::Vertex> vertices {
-			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-			{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+	std::unique_ptr<EveModel> createCubeModel(EveDevice& device, glm::vec3 offset) {
+		std::vector<EveModel::Vertex> vertices{
+	
+			// left face (white)
+			{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+			{{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+			{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+		
+			// right face (yellow)
+			{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+			{{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+			{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+		
+			// top face (orange, remember y axis points down)
+			{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+			{{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+			{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+		
+			// bottom face (red)
+			{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+			{{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+			{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+		
+			// nose face (blue)
+			{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+			{{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+			{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+		
+			// tail face (green)
+			{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+			{{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+			{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+	
 		};
-		auto eveModel = std::make_shared<EveModel>(eveDevice, vertices);
+		for (auto& v : vertices) {
+			v.position += offset;
+		}
+		return std::make_unique<EveModel>(device, vertices);
+	}
 
-		auto triangle = EveGameObject::createGameObject();
-		triangle.model = eveModel;
-		triangle.color = {.1f, .8f, .1f};
-		triangle.transform2d.translation.x = .2f;
-		triangle.transform2d.scale = {2.f, .5f};
-		triangle.transform2d.rotation = .25f * glm::two_pi<float>();
+	void App::loadGameObjects() {
+		std::shared_ptr<EveModel> eveModel = createCubeModel(eveDevice, {0.f, 0.f, 0.f});
 
-		gameObjects.push_back(std::move(triangle));
+		auto cube = EveGameObject::createGameObject();
+		cube.model = eveModel;
+		cube.transform.translation = {.0f, .0f, .5f};
+		cube.transform.scale = {.5f, .5f, .5f};
+		gameObjects.push_back(std::move(cube));
 	}
 }
--- src/engine/eve_game_object.hpp
+++ src/engine/eve_game_object.hpp
@@ -2,48 +2,78 @@
 
 #include "eve_model.hpp"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <memory>
 
-namespace eve {
+namespace eve
+{
 
-	struct Transform2dComponent {
-		glm::vec2 translation{};
-		glm::vec2 scale{1.f, 1.f};
-		float rotation;
+	struct TransformComponent
+	{
+		glm::vec3 translation{};
+		glm::vec3 scale{1.f, 1.f, 1.f};
+		glm::vec3 rotation{};
 
-		glm::mat2 mat2() {
-			const float s = glm::sin(rotation);
-			const float c = glm::cos(rotation);
-			glm::mat2 rotMatrix{{c, s}, {-s, c}};
-
-			glm::mat2 scaleMatrix{{scale.x, .0f}, {.0f, scale.y}};
-			return rotMatrix * scaleMatrix; 
+		// Matrix corrsponds to Translate * Ry * Rx * Rz * Scale
+		// Rotations correspond to Tait-bryan angles of Y(1), X(2), Z(3)
+		// https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
+		glm::mat4 mat4()
+		{
+			const float c3 = glm::cos(rotation.z);
+			const float s3 = glm::sin(rotation.z);
+			const float c2 = glm::cos(rotation.x);
+			const float s2 = glm::sin(rotation.x);
+			const float c1 = glm::cos(rotation.y);
+			const float s1 = glm::sin(rotation.y);
+			return glm::mat4{
+				{
+					scale.x * (c1 * c3 + s1 * s2 * s3),
+					scale.x * (c2 * s3),
+					scale.x * (c1 * s2 * s3 - c3 * s1),
+					0.0f,
+				},
+				{
+					scale.y * (c3 * s1 * s2 - c1 * s3),
+					scale.y * (c2 * c3),
+					scale.y * (c1 * c3 * s2 + s1 * s3),
+					0.0f,
+				},
+				{
+					scale.z * (c2 * s1),
+					scale.z * (-s2),
+					scale.z * (c1 * c2),
+					0.0f,
+				},
+				{translation.x, translation.y, translation.z, 1.0f}};
 		}
 	};
 
-	class EveGameObject {
-		public:
-			using id_t = unsigned int;
+	class EveGameObject
+	{
+	public:
+		using id_t = unsigned int;
 
+		static EveGameObject createGameObject()
+		{
+			static id_t currentId = 0;
+			return EveGameObject{currentId++};
+		}
 
-			static EveGameObject createGameObject() {
-				static id_t currentId = 0;
-				return EveGameObject{currentId++};
-			}
+		EveGameObject(const EveGameObject &) = delete;
+		EveGameObject &operator=(const EveGameObject &) = delete;
+		EveGameObject(EveGameObject &&) = default;
+		EveGameObject &operator=(EveGameObject &&) = default;
 
-			EveGameObject(const EveGameObject &) = delete;
-			EveGameObject &operator=(const EveGameObject &) = delete;
-			EveGameObject(EveGameObject &&) = default;
-			EveGameObject &operator=(EveGameObject &&) = default;
+		id_t getId() { return id; }
 
-			id_t getId() { return id; }
+		std::shared_ptr<EveModel> model{};
+		glm::vec3 color{};
+		TransformComponent transform{};
 
-			std::shared_ptr<EveModel> model{};
-			glm::vec3 color{};
-			Transform2dComponent transform2d{};
-		private:
-			EveGameObject(id_t objId) : id{objId} {}
+	private:
+		EveGameObject(id_t objId) : id{objId} {}
 
-			id_t id;
+		id_t id;
 	};
 }
