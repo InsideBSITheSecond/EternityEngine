4bfe737283266fc94ef44f04719f149b24028d68
InsideBSI <insidebsiprod@gmail.com>
2024-02-07 22:29:49 +0100

    load 3d models

--- src/engine/eve_model.cpp
+++ src/engine/eve_model.cpp
@@ -1,7 +1,27 @@
 #include "eve_model.hpp"
+#include "eve_utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <cassert>
 #include <string.h>
+#include <iostream>
+#include <unordered_map>
+
+namespace std {
+	template <>
+	struct hash<eve::EveModel::Vertex> {
+		size_t operator()(eve::EveModel::Vertex const &vertex) const {
+			size_t seed = 0;
+			eve::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}
 
 namespace eve
 {
@@ -21,6 +41,14 @@ namespace eve
 			vkDestroyBuffer(eveDevice.device(), indexBuffer, nullptr);
 			vkFreeMemory(eveDevice.device(), indexBufferMemory, nullptr);
 		}
+	}
+
+	std::unique_ptr<EveModel> EveModel::createModelFromFile(EveDevice &device, const std::string &filepath)
+	{
+		Builder builder{};
+		builder.loadModel(filepath);
+		std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+		return std::make_unique<EveModel>(device, builder);
 	}
 
 	void EveModel::createVertexBuffers(const std::vector<Vertex> &vertices)
@@ -141,5 +169,71 @@ namespace eve
 		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 		return attributeDescriptions;
+	}
+
+	void EveModel::Builder::loadModel(const std::string &filepath)
+	{
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str()))
+		{
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+		for (const auto &shape : shapes)
+		{
+			for (const auto &index : shape.mesh.indices)
+			{
+				Vertex vertex{};
+
+				if (index.vertex_index >= 0)
+				{
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2]};
+					auto colorIndex = 3 * index.vertex_index + 2;
+					if (colorIndex < attrib.colors.size())
+					{
+						vertex.color = {
+							attrib.colors[colorIndex - 2],
+							attrib.colors[colorIndex - 1],
+							attrib.colors[colorIndex - 0]};
+					} else {
+						vertex.color = {1.f, 1.f, 1.f};
+					}
+				}
+
+				if (index.normal_index >= 0)
+				{
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2]};
+				}
+
+				if (index.texcoord_index >= 0)
+				{
+					vertex.uv = {
+						attrib.texcoords[3 * index.texcoord_index + 0],
+						attrib.texcoords[3 * index.texcoord_index + 1],
+					};
+				}
+
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 }
--- src/engine/eve_model.hpp
+++ src/engine/eve_model.hpp
@@ -7,21 +7,30 @@
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <memory>
 
 namespace eve {
 	class EveModel {
 		public:			
 			struct Vertex {
-				glm::vec3 position;
-				glm::vec3 color;
+				glm::vec3 position{};
+				glm::vec3 color{};
+				glm::vec3 normal{};
+				glm::vec2 uv{};
 
 				static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 				static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+				bool operator==(const Vertex &other) const {
+					return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+				}
 			};
 
 			struct Builder {
 				std::vector<Vertex> vertices{};
 				std::vector<uint32_t> indices{};
+
+				void loadModel(const std::string &filepath);
 			};
 			
 			EveModel(EveDevice &device, const EveModel::Builder &builder);
@@ -29,6 +38,8 @@ namespace eve {
 
 			EveModel(const EveModel&) = delete;
 			EveModel &operator=(const EveModel&) = delete;
+
+			static std::unique_ptr<EveModel> createModelFromFile(EveDevice &device, const std::string &filepath);
 
 			void bind(VkCommandBuffer commandBuffer);
 			void draw(VkCommandBuffer commandBuffer);
