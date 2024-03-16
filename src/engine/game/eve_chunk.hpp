4616b4e461d8394f0b6607b1fca96084c40ce911
InsideBSI <insidebsiprod@gmail.com>
2024-03-16 18:00:28 +0100

    redondant include

--- src/engine/game/eve_chunk.hpp
+++ src/engine/game/eve_chunk.hpp
@@ -7,7 +7,6 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/gtc/matrix_transform.hpp>
 #include "glm/ext.hpp"
-#include <glm/gtc/matrix_transform.hpp>
 
 #include <boost/thread/thread.hpp>
 #include <boost/range/join.hpp>
